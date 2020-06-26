9fdd0650226da952bc1fc9a004a779e15cf64c26
caseyhan/X2017522041 <1124011866@qq.com>
2020-06-26 23:36:30 +0800

    Update settings.py

--- 项目/settings.py
+++ 项目/settings.py
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT= '/data/static/'  # 配置收集静态资源
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
